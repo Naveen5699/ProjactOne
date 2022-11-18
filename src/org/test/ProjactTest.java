8bd28013148773fea01abc74dd40e0bec8a27c0b
Naveen5699 <naveenkumar561999@gmail.com>
2022-11-18 16:57:05 +0530

    added code

---
+++ src/org/test/ProjactTest.java
@@ -0,0 +1,15 @@
+package org.test;
+
+public class ProjactTest {
+	
+	private void add() {
+    System.out.println("Naveen");
+    System.out.println("5699");
+	}
+	public static void main(String[] args) {
+		ProjactTest n = new ProjactTest();
+		
+		n.add();
+	}
+
+}
